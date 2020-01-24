81f19e468bf7fcedeb821fa30312bfe3d6b65ac7
Ian <icc@S9-C02ZP0GCJV3X.local>
2020-01-24 08:28:34 -0500

    added url fetching recipe

--- MicrosoftEdge/MicrosoftEdgeChromiumURLProvider.py
+++ MicrosoftEdge/MicrosoftEdgeChromiumURLProvider.py
@@ -17,8 +17,105 @@
 from autopkglib import Processor, ProcessorError
 import urllib.request as urllib
 
-EDGE_URL_FEED = "https://edgeupdates.microsoft.com/api/products?view=enterprise"
+MSEDGE_URL_FEED = "https://edgeupdates.microsoft.com/api/products?view=enterprise"
 
 __all__ = ["MicrosoftEdgeChromiumURLProvider"]
 
+#Define Options
+PLATFORM_OPT = [
+    "Windows",
+    "MacOS"#,
+    # Commenting out 'Any' as it should not be needed in production
+    #"Any"
+    ]
+ARCHITECTURE_OPT = ["x86","x64"]
+PRODUCT_OPT = [
+    "Dev",
+    "Beta",
+    "Stable"#,
+    # Commenting out 'EdgeUpdate' and 'Policy' as they should not be needed in production
+    #"EdgeUpdate",
+    # "Policy"
+    ]
+
 class MicrosoftEdgeChromiumURLProvider(Processor)
+    ("Scrap Microsoft's feed for the url path to the latest release of Microsoft Edge (Chromium).")
+    description = __doc__
+    input_variables = {
+        "PLATFORM": {
+            "required": True,
+            "default": "Windows",
+            "description": (
+                "Which Product / Branch to download. "
+                "Options: {}".format(PLATFORM_OPT)
+            )
+        },
+        "ARCHITECTURE": {
+            "required": False,
+            "default": "x64",
+            "description": (
+                "Which Architecture to download. "
+                "Options: {}".format(ARCHITECTURE_OPT)
+            )
+        },
+        "PRODUCT": {
+            "required": False,
+            "default": "Stable",
+            "description": (
+                "Which Product / Branch to download. "
+                "Options: {}".format(PRODUCT_OPT)
+            )
+        }
+    }
+    output_variables = {
+        "url": {"description": "The URL of the latest released version of the given branch"},
+        "installer_version": {"description": "Platform and version details as a concatenated string."},
+        "Hash": {"description": "The value of the installer hash"},
+        "HashAlgorithm": {"description": "The algorithm type of the hash"},
+        "SizeInBytes": {"description": "The size in bytes of the installer"},
+        "installer_type": {"description": "The file type of the installer"},
+        "PublishedTime": {"description": "The time when the installer was published"}
+    }
+
+    def main(self):
+        self.output("Available Platforms: {}".format(PLATFORM_OPT))
+        platform = self.env.get("PLATFORM")
+
+        self.output("Available Architectures: {}".format(ARCHITECTURE_OPT))
+        architecture = self.env.get("ARCHITECTURE") or self.input_variables["ARCHITECTURE"]["default"]
+
+        self.output("Available Product Branches: {}".format(PRODUCT_OPT))
+        product = self.env.get("PRODUCT") or self.input_variables["PRODUCT"]["default"]
+
+        #installer_platform = platform + "-" + architecture + "-" + product
+
+        blob = urllib.urlopen(MSEDGE_URL_FEED)
+        feed_json = json.loads(blob)
+        blob.close()
+
+        # Select array item by product name
+        for item in feed_json:
+            if item["Product"] == product:
+                selected_product = item
+
+        # Select Architecture and and Platform
+        releases = []
+        for release in selected_product:
+            if ((release["Platform"] == platform ) and (release["Architecture"] == architecture)):
+                releases.add(release)
+
+        # Sort releases, and select latest released
+        latest_release = sorted(releases, key=lambda x: x["ProductVersion"], reverse=True )[0]
+
+        # Return Values
+        self.env["url"] = latest_release["Artifacts"][0]["Location"]
+        self.env["installer_version"] = platform + "-" + architecture + "-v" + latest_release["ProductVersion"] + "-"+ product
+        self.env["Hash"] = latest_release["Artifacts"][0]["Hash"]
+        self.env["HashAlgorithm"] = latest_release["Artifacts"][0]["HashAlgorithm"]
+        self.env["SizeInBytes"] = latest_release["Artifacts"][0]["SizeInBytes"]
+        self.env["installer_type"] = latest_release["Artifacts"][0]["ArtifactName"]
+        self.env["PublishedTime"] = latest_release["PublishedTime"]
+
+if __name__ == "__main__":
+    PROCESSOR = MicrosoftEdgeChromiumURLProvider()
+    PROCESSOR.execute_shell()
